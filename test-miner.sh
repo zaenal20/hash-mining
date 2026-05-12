4da41ffff12f57d7759c4d1e8cb75e7b8291e50c
Zaenal20 <zaenalariffin04@gmail.com>
2026-05-12 17:33:37 +0700

    -

--- test-miner.sh
+++ test-miner.sh
@@ -23,14 +23,14 @@ echo -e "\nCompile sukses! Menjalankan verifikasi...\n"
 CHALLENGE="5bcc23a21849ceebf4885d7580babf6cfbbaec4a66941e36d57a2946ad2ad2b1"
 
 # Hash asli yang ketemu adalah 0x000000000000125b...
-# Kita pasang target sedikit di atasnya biar dianggap "sukses" oleh miner lu
+# pasang target sedikit di atasnya biar dianggap "sukses" oleh miner lu
 TARGET="000000000000125c000000000000000000000000000000000000000000000000"
 
 # Nonce pemenang aslinya adalah berakhiran ...dc65
-# Kita mulai pencarian dari ...dc01 (sekitar 100 langkah sebelum menang)
+# mulai pencarian dari ...dc01 (sekitar 100 langkah sebelum menang)
 START_NONCE="b3beebac11185eda01b9832e4675e41c430a31e09a32a246000000066140dc01"
 
-# Kita jalankan: 
+# jalankan: 
 # - 1 grid
 # - 1 block (thread tunggal biar gampang ditrack)
 # - 256 nonces per batch (ini lebih dari cukup untuk cover range 0x00 sampai 0xff)
